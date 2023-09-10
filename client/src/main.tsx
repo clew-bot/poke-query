cf336bc2eb5d0916da23efe5f8b804844b03e7bf
Chad Lew <lewchad11@gmail.com>
2023-09-09 22:25:30 -0700

    setup react-query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import SingleMon from "./pages/SingleMon.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/pokemon/:id",
+    element: <SingleMon />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
---
+++ client/src/pages/SingleMon.tsx
@@ -0,0 +1,7 @@
+const SingleMon = () => {
+  return (
+    <div>Details about a single Pokemon</div>
+  )
+}
+
+export default SingleMon
